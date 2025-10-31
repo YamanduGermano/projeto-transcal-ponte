a0467f477c4c50dffaa7a925fd0e65844cf82ad0
Yamandú Germano Cavalcanti <yamandu.germano@gmail.com>
2025-10-31 11:39:45 -0300

    arquivo matlab

---
+++ software.m
@@ -0,0 +1,19 @@
+clear
+close
+clc
+
+n_nos = 3;
+n_gdl = n_nos*2;
+relacoes_elementos = [
+    [1 2 3 4]
+    [3 4 5 6]
+    [1 2 5 6]
+];
+
+kg = zeros(n_gdl, n_gdl);
+
+for i = 1:n_nos+1
+    
+end
+
+kg
